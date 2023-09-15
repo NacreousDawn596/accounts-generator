98fa427009501543e13a30447ff5edfbff4e3540
NacreousDawn596 <75474839+NacreousDawn596@users.noreply.github.com>
2023-09-15 13:55:49 +0100

    idk what I'm doing

--- spotify/data-to-json.py
+++ spotify/data-to-json.py
@@ -1,8 +1,5 @@
 import json, sys
-datas = open(sys.argv[1], "r").read().splitlines()
-
-idk = json.dumps([{"nickname": i.split(":")[0], "username": i.split(":")[1], "email": i.split(":")[2], "password": i.split(":")[3]} for i in datas], indent=4)
 
 u = open(f'{sys.argv[1].split(".")[0]}.json', 'w')
-u.write("".join(idk))
+u.write("".join(json.dumps([{"nickname": i.split(":")[0], "username": i.split(":")[1], "email": i.split(":")[2], "password": i.split(":")[3]} for i in open(sys.argv[1], "r").read().splitlines()], indent=4)))
 u.close()
